970062e98044b8779c259caefd1f3c7d5b1956b9
Jamal Ahmed <jamal.ahmed@student.moringaschool.com>
2024-09-11 10:22:01 +0300

    complete lab

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,18 @@
 // Your code here
+// utils.js
+
+// Function to calculate the age of a person based on the birth year
+function currentAgeForBirthYear(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+}
+
+// Exporting the function (CommonJS format)
+module.exports = { currentAgeForBirthYear };
+
+// OR, if using ES6 modules:
+// export function currentAgeForBirthYear(birthYear) {
+//   const currentYear = new Date().getFullYear();
+//   return currentYear - birthYear;
+// }
+
